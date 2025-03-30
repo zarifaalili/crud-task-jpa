7efc40d4cf640cde8473ddfc58e78e03ab2ba790
zarifaalili <alilizarifa@gmail.com>
2025-03-30 12:44:48 +0400

    remota bagladiq

---
+++ src/main/java/org/example/crudtaskjpa/CrudTaskJpaApplication.java
@@ -0,0 +1,13 @@
+package org.example.crudtaskjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTaskJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTaskJpaApplication.class, args);
+    }
+
+}
